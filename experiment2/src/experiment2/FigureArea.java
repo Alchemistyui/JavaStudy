ac893148cf5bac0144ef0452e4e81836a6a55e10
Alchemist <1319525622@qq.com>
2017-11-30 08:59:15 +0800

    new experiment

---
+++ experiment2/src/experiment2/FigureArea.java
@@ -0,0 +1,45 @@
+package experiment2;
+
+public abstract class FigureArea {
+
+	int measure1, measure2;
+	
+	 abstract int size();
+	 
+	 public void main(int[] args) {
+		 Rectangle rect = new Rectangle(4, 5);
+		 Triangle tri = new Triangle(4, 5);
+		 
+		 System.out.println("the area of rect is:" + rect.size());
+		 System.out.println("the area of tri is:" + tri.size());
+	 }
+}
+
+class Rectangle extends FigureArea {
+
+	Rectangle(int width, int height) {
+		measure1 = width;
+		measure2 = height;
+	}
+
+	@Override
+	int size() {
+		
+		return measure1 * measure2;
+	}
+	
+}
+
+class Triangle extends FigureArea {
+	
+	Triangle(int bottom, int height) {
+		measure1 = bottom;
+		measure2 = height;
+	}
+	
+	@Override
+	int size() {
+		
+		return measure1 * measure2 / 2;
+	}
+}
