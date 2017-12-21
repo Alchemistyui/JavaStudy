d7b2441cc348626f35b0dcc0e2ca57e6b5d73a36
Alchemist <1319525622@qq.com>
2017-12-21 21:28:09 +0800

    review Chapter 7

---
+++ ReviewProgram/src/Chapter7/Exception/ExceptionTest.java
@@ -0,0 +1,37 @@
+package Chapter7.Exception;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			int a = 0;
+			int b = 9 / 0;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Exception message: " + e.getMessage());
+			System.out.println("Exception message: " + e.toString());
+		}
+		
+		try {
+			throwException();
+		}
+		catch (NumberOverTen e){
+			System.out.println("catch NumberOverTen");
+		}
+		
+	}
+
+	static void throwException() throws NumberOverTen {
+		throw new NumberOverTen();
+	}
+	
+}
+
+
+class NumberOverTen extends Throwable {
+	NumberOverTen() {
+		System.out.println("number should less than ten..");
+	}
+}
