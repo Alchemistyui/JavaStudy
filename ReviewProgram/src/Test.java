18090d3cc55ed3c3cd069777168608fb234bec3b
Alchemist <1319525622@qq.com>
2017-12-31 11:45:10 +0800

    test

---
+++ ReviewProgram/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//
+//		int x = 1, y = 2, z = 3;
+//		y += z-- / ++x;
+//		System.out.println(y);
+		String x = null;
+		
+		int y = (x!=null)&&(x.length()>0)?x.length() : 0;
+		
+		System.out.println(y);
+	}
+
+}
