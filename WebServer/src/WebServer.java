f0ee82811ccf75d4b013fd67a216736d45ee6980
Alchemist <1319525622@qq.com>
2018-04-25 12:39:14 +0800

    edit

--- WebServer/src/WebServer.java
+++ WebServer/src/WebServer.java
@@ -34,6 +34,7 @@ public class WebServer {
 			try {
 				// 打得到请求网页名
 				resource = url.split(" ")[1];
+				System.out.println(resource);
 				// 设置默认网页为index.html
 				if (resource.equals("/")) {
 					resource = "index.html";
@@ -48,30 +49,49 @@ public class WebServer {
 			}
 
 			String newPath = path + resource;
-			
-				// 读取服务器本地文件
-				File file = new File(newPath);
 
-				BufferedWriter fbw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
+			// 读取服务器本地文件
+			File file = new File(newPath);
 
-				if (!file.exists()) {
-					fbw.write("HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n"
-	                        + "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>");
-					System.out.println("404");
-					continue;
-				}
-				
+			BufferedWriter fbw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
+
+			// if (!file.exists()) {
+			// String head = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type:
+			// text/html\r\n";
+			// head = new String(head.getBytes("utf-8"),"utf-8");
+			// fbw.write(head + "\r\n" + "<html><head></head><body><h1>404 Not
+			// Found</h1></body></html>\\r\\n");
+			// System.out.println("404");
+			//// continue;
+			// }
+			// else {
+			// System.out.println(file.getName());
+			// }
+			try {
 				BufferedReader fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-				
+
 				String readLine = null;
+				String contentType = "";
 				int index = resource.indexOf(".");
 				String type = resource.substring(index + 1);
 				// System.out.println(type);
 				// System.out.println("html ..."+resource.substring(index));
 				// 标准头部
-//				 if(type == "html") {
-				fbw.write("HTTP/1.1 200 OK\r\n" + "Content-Type:text/html\r\n" + "\r\n");
-//				 }
+				if (type == "html") {
+					contentType = "text/html;charset=utf-8";
+				} else if (type == "png") {
+					contentType = "image/png";
+				} else if (type == "jpg") {
+					contentType = "image/jpeg";
+				} else if (type == "css") {
+					contentType = "text/css";
+				} else if (type == "js") {
+					contentType = "application/x-javascript";
+				}
+				String head = "HTTP/1.1 200 OK\r\n" + "Content-Type:" + contentType + "\r\n" + "\r\n";
+				head = new String(head.getBytes("gbk"), "utf-8");
+				// fbw.write(head);
+				// }
 				// if(type == "html") {
 				//// System.out.println("html");
 				// fbw.write("Content-Type:text/html\r\n");
@@ -85,11 +105,13 @@ public class WebServer {
 
 				// System.out.println("emmmm");
 				// 一行一行读取文件并输出到网页
+				String data = "";
+				// = fbr.read();
 				while ((readLine = fbr.readLine()) != null) {
-					fbw.write(readLine);
-					fbw.newLine();
+					data += readLine;
+					// fbw.newLine();
 				}
-
+				fbw.write(head + data);
 				fbw.flush();
 
 				fbw.close();
@@ -97,11 +119,19 @@ public class WebServer {
 				fbr.close();
 
 				accept.close();
-//			} catch (FileNotFoundException e) {
-//				String errorMessage = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n"
-//                        + "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>";
-//				fbw.write(errorMessage.getBytes());
-//			}
+				// } catch (FileNotFoundException e) {
+				// String errorMessage = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type:
+				// text/html\r\n"
+				// + "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>";
+				// fbw.write(errorMessage.getBytes());
+				// }
+
+			} catch (FileNotFoundException e) {
+				String head = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n";
+				 head = new String(head.getBytes("utf-8"),"utf-8");
+				 fbw.write(head + "\r\n" + "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n");
+				 System.out.println("404");
+			}
 		}
 	}
 }
