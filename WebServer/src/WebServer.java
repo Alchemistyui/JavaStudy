707e53795f5728c8519de19fc380588813dd984a
Alchemist <1319525622@qq.com>
2018-04-28 20:52:04 +0800

    finish web server

--- WebServer/src/WebServer.java
+++ WebServer/src/WebServer.java
@@ -13,77 +13,65 @@ import java.net.Socket;
 import java.io.FileNotFoundException;
 
 public class WebServer {
+	int port = 8888;
+	String path = "/Users/ryshen/Desktop/编程/WebStudy/webServerResource";
+
+	String getResource() {
+		
+	}
 
 	public static void main(String[] args) throws IOException {
-		String path = "/Users/ryshen/Desktop/编程/WebStudy/webServerResource/";
-		ServerSocket serverSocket = new ServerSocket(8888);
-
+		// 定义服务器文件所在路径
+		
+		// 创建绑定到特定端口的服务器套接字
+		ServerSocket serverSocket = new ServerSocket(port);
 		System.out.println("Serving HTTP  ...");
-
+		// 服务器一直提供服务
 		while (true) {
+			// 接受到套接字的连接
 			Socket accept = serverSocket.accept();
-
+ 			// 通过输入管道获取HTTP请求报文
 			InputStream inputStream = accept.getInputStream();
 
-			// 得到请求报文第一行
+			// 包装输入管道，提高速度
 			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+			// 得到请求报文第一行
 			String url = br.readLine();
-
-			// System.out.println("url ..."+url);
 
 			String resource = "";
 			try {
 				// 取得请求资源路径
 				resource = url.split(" ")[1];
+				// 查看请求资源
 				System.out.println(resource);
 				// 设置默认网页为index.html
 				if (resource.equals("/")) {
-					resource = "index.html";
+					resource = "/index.html";
 				}
-
-				// int index2 = url.indexOf(" ", index);
-				// System.out.println("index "+index);
-				// System.out.println("html ..."+resource.substring(index));
-
 			} catch (Exception e) {
 				continue;
 			}
-//			获取本地绝对路径
+			//	获取本地绝对路径
 			String newPath = path + resource;
-
-			
-			// if (!file.exists()) {
-			// String head = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type:
-			// text/html\r\n";
-			// head = new String(head.getBytes("utf-8"),"utf-8");
-			// fbw.write(head + "\r\n" + "<html><head></head><body><h1>404 Not
-			// Found</h1></body></html>\\r\\n");
-			// System.out.println("404");
-			//// continue;
-			// }
-			// else {
-			// System.out.println(file.getName());
-			// }
-			BufferedOutputStream fbw = null;
-			BufferedInputStream fbr = null;
+			// 读取本地文件的输入管道及本地文件输出到浏览器的管道
+			// 此处只能使用字节流，因为字符流在处理图片时不能显示，图片中的字符变多，可能因为读取了多余的空白字符
+			BufferedOutputStream fos = null;
+			BufferedInputStream fis = null;
 			try {
 				
 				// 读取服务器本地文件
 				File file = new File(newPath);
-//				建立写管道
-				fbw = new BufferedOutputStream(accept.getOutputStream());
-
-				fbr = new BufferedInputStream(new FileInputStream(file));
-//				读取获得所需请求资源类型
+				// 建立读写管道
+				fos = new BufferedOutputStream(accept.getOutputStream());
+				fis = new BufferedInputStream(new FileInputStream(file));
+				// 读取获得所需请求资源类型
 				String readLine = null;
 				String contentType = "";
+				// 找到第一个.的索引并提取后面类型的字符串
 				int index = resource.indexOf(".");
 				String type = resource.substring(index + 1);
-				// System.out.println(type);
-				// System.out.println("html ..."+resource.substring(index));
 				
-				
-				// 响应报文标准头部
+				// 判断请求资源类型来填充不同的报文首部
 				if (type.equals("html")) {
 					contentType = "text/html;charset=utf-8";
 				} else if (type.equals("png")) {
@@ -95,62 +83,40 @@ public class WebServer {
 				} else if (type.equals("js")) {
 					contentType = "application/x-javascript";
 				}
+				// 响应报文标准头部，并将其utf8编码
 				String head = "HTTP/1.1 200 OK\r\n" + "Content-Type:" + contentType + "\r\n" + "\r\n";
 				head = new String(head.getBytes("utf-8"), "utf-8");
-				 fbw.write(head.getBytes());
-				// }
-				// if(type == "html") {
-				//// System.out.println("html");
-				// fbw.write("Content-Type:text/html\r\n");
-				// }
-				// else if (type == "png") {
-				// fbw.write("HTTP/1.1 200 OK\r\n"
-				// + "Content-Type:image/png\r\n"
-				// + "\r\n");
-				// }
-				// fbw.write("\r\n");
-
-				// System.out.println("emmmm");
+				// 向管道写响应报文头部
+				fos.write(head.getBytes());
 				
-				 int len = 0;
-		            //byte[] bytes = new byte[inputStream .available()];
-		            byte[] bytes = new byte[1024];
-		            while ((len = fbr.read(bytes)) != -1) {
-		                fbw.write(bytes);
-		                fbw.flush();
-		            }
-				// 一行一行读取文件
-//				int data = 0;
-//				 data = fbr.read();
-////				while ((readLine = fbr.readLine()) != null) {
-////					data += readLine;
-////					// fbw.newLine();
-////				}
-////				发送响应报文
-//				fbw.write(head + data);
-				
-				// } catch (FileNotFoundException e) {
-				// String errorMessage = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type:
-				// text/html\r\n"
-				// + "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>";
-				// fbw.write(errorMessage.getBytes());
-				// }
-
-			} catch (FileNotFoundException e) {
-				String head = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n";
-				 head = new String(head.getBytes("utf-8"),"utf-8");
-				 byte[] bytes = new byte[1024];
-				 bytes = (head + "\r\n" + "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n").getBytes();
-				 fbw.write(bytes);
-				 System.out.println("404");
-			} finally {
-				fbw.flush();
-
-				fbw.close();
-				if (fbr != null) {
-					fbr.close();
+				// 采用read(byte[] b, int off, int len)方法，因为若采用read(byte[] b)方法，
+				// 最后不满1024的时候每次写都会填满缓冲区，造成页面中多余其他元素
+				// 而用现在这种方法，读到最后一次的时候就只是写实际的字节数
+				int len = 0;
+				byte[] bytes = new byte[1024];
+				while ((len = fis.read(bytes)) != -1) {
+					fos.write(bytes, 0, len);
+					fos.flush();
 				}
 
+				// 若请求不存在的文件则返回404报文
+			} catch (FileNotFoundException e) {
+				String head = "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n";
+				head = new String(head.getBytes("utf-8"),"utf-8");
+				byte[] bytes = new byte[1024];
+				bytes = (head + "\r\n" + "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n").getBytes();
+				fos.write(bytes);
+				System.out.println("404");
+			} finally {
+				// 最后关闭各种管道
+				fos.flush();
+
+				fos.close();
+				// 如果文件不存在则不存在fis的关闭
+				if (fis != null) {
+					fis.close();
+				}
+				// 连接关闭
 				accept.close();
 			}
 		}
